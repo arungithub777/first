507b9c4eef78db9d800a7ea72f518c0ccb2d0d53
arungithub777 <70995191+arungithub777@users.noreply.github.com>
2020-09-15 15:12:40 +1000

    Add files via upload

---
+++ my first project.py
@@ -0,0 +1,12 @@
+a=input("What's your name? ")
+print("My name is ", a)
+
+def add(a,b):
+    c=a+b
+    return print("The addition is", c)
+def miles(a):
+    b=0.621371*a
+    return print("The miles is",b)
+
+add(3,5)
+miles(10)
